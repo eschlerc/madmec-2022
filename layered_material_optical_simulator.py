365aa0a9220cffa6b03158d5f98cd0c78eb3aca3
Chris Eschler <eschlerc@mit.edu>
2022-07-25 15:10:18 -0400

    Effective medium approximation

--- layered_material_optical_simulator.py
+++ layered_material_optical_simulator.py
@@ -49,6 +49,32 @@ def import_nk_data(filename, material):
     imported_materials.append(material)
 
 
+# Calculate n,k from a function of wavelength (um) and add it to nk_interp
+def import_from_eqn(function, material):
+    global nk_interp
+    nk_interp[material] = function(lambda_list)
+
+
+# ITO n,k model from Del Villar et al. 2010
+def ito_nk_eqn(wl):
+    om = 2*np.pi*3e8/(wl*1e-6)
+    return(np.sqrt(3.57 - 1.89e15**2/(om**2 + 1j*om/6.34e-15) + 
+                   0.49*5.61e15**2/(5.61e15**2 - om**2 - 1j*9.72e13*om)))
+
+
+# Effective medium approximation based on Maxwell Garnet equation
+# Probably a poor approximation, but easy to implement
+# Used for small volume fraction of inclusions in a matrix
+# matrix_material: string name of matrix material from list of imported materials
+# inclusion_material: string name of inclusion material " "
+# vol_frac: volume fraction of inclusions
+def effective_medium(matrix_material, inclusion_material, vol_frac):
+    eps_matrix = nk_interp[matrix_material]**2
+    eps_inclusion = nk_interp[inclusion_material]**2
+    return np.sqrt(eps_matrix*(2*vol_frac*(eps_inclusion-eps_matrix)+eps_inclusion+2*eps_matrix)/
+                  (2*eps_matrix+eps_inclusion-vol_frac*(eps_inclusion-eps_matrix)))
+
+
 # Interpolate to all the wavelength values given in lambda_list using
 # quadratic interpolation. Can handle n and k lists with different wavelengths
 # as input.
@@ -56,9 +82,11 @@ def interp_nk_data(material):
     # Look only at the data for desired material
     # dropna() allows for processing of different length n and k data
     group = nk_data.loc[nk_data['material']==material]
-    n_interp = interp1d(group['wl'].dropna(), group['n'].dropna(), kind='quadratic', 
+    n_data = group[['wl', 'n']].dropna()
+    k_data = group[['wl.1', 'k']].dropna()
+    n_interp = interp1d(n_data['wl'], n_data['n'], kind='quadratic', 
                         bounds_error=False, fill_value='extrapolate')
-    k_interp = interp1d(group['wl.1'].dropna(), group['k'].dropna(), kind='quadratic',
+    k_interp = interp1d(k_data['wl.1'], k_data['k'], kind='quadratic',
                         bounds_error=False, fill_value='extrapolate')
     # Combine n and k into complex-valued refrac indices at each wavelength
     nk_interp[material] = (np.maximum(0,n_interp(lambda_list)) + 
@@ -160,18 +188,44 @@ import_nk_data('Refractive_indices/nk_aLi0_18WO3.csv', 'a-Li0.18WO3')   # 0.3-2.
 import_nk_data('Refractive_indices/nk_aLi0_34WO3.csv', 'a-Li0.34WO3')   # 0.3-2.5 um    !!
 import_nk_data('Refractive_indices/nk_ins-VO2.csv', 'VO2-lowT')         # 0.3-15 um
 import_nk_data('Refractive_indices/nk_met-VO2.csv', 'VO2-highT')        # 0.3-15 um
+import_nk_data('Refractive_indices/nk_Ag.csv', 'Ag')                    # 0.27-24.9 um
+import_from_eqn(ito_nk_eqn, 'ITO_eqn')
 
+# print(nk_data.loc[nk_data['material']=='Ag'])
 # Interpolate all the imported materials
 for material in imported_materials:
     interp_nk_data(material)
 
-plot_nk_interp('Eagle XG')
+
+eff = effective_medium('PMMA', 'Ag', 0.1)
+eff.min()
+
+plt.figure()
+plt.plot(lambda_list, nk_interp['Ag'].real, 'b-', label='n Ag')
+plt.plot(lambda_list, nk_interp['Ag'].imag, 'b--', label='k Ag')
+plt.plot(lambda_list, nk_interp['PMMA'].real, 'r-', label='n PMMA')
+plt.plot(lambda_list, nk_interp['PMMA'].imag, 'r--', label='k PMMA')
+plt.plot(lambda_list, eff.real, 'k-', label='n$_{eff}$')
+plt.plot(lambda_list, eff.imag, 'k--', label='k$_{eff}$')
+plt.xlabel('Wavelength ($\mu$m)')
+plt.xlim([0.3, 15])
+plt.xscale('symlog', linthresh=1)
+plt.xticks(tick_list, tick_labels)
+plt.ylabel('Material n,k')
+plt.ylim([0, 5])
+plt.legend()
+plt.title('20% AgNW in PMMA')
+plt.show()
+
+
+plot_nk_interp('Ag')
+
     
 # print(min(nk_interp['aWO3'].imag)) # Check min k value, ensure non-negative
 
 # Set up multilayer stack layout
-material_list = ['Air', 'TiO2', 'VO2-highT', 'Eagle XG', 'Air'] # List of materials from outside to inside
-d_list = [np.inf, 0.05, 0.05, 700., np.inf]  # Thickness of each layer, in um
+material_list = ['Air', 'Silica', 'VO2-lowT', 'Eagle XG', 'Air'] # List of materials from outside to inside
+d_list = [np.inf, 0.167, 0.04, 700., np.inf]  # Thickness of each layer, in um
 c_list = ['i', 'c', 'c', 'i', 'i']          # 'c' for coherent, 'i' for incoherent layer
 
 # Incoming light
@@ -180,25 +234,3 @@ plot_atr(material_list, d_list, c_list, 0, title='Incoming light')
 
 # Outgoing light, uses [::-1] to reverse lists
 plot_atr(material_list[::-1], d_list[::-1], c_list[::-1], 0, title='Outgoing light')
-
-
-# Testing with ITO n,k model from Del Villar et al. 2010
-def ito_nk(wl):
-    om = 2*np.pi*3e8/(wl*1e-6)
-    return(np.sqrt(3.57 - 1.89e15**2/(om**2 + 1j*om/6.34e-15) + 0.49*5.61e15**2/(5.61e15**2 - om**2 - 1j*9.72e13*om)))
-
-plt.figure()
-plt.plot(lambda_list, ito_nk(lambda_list).real)
-plt.plot(lambda_list, ito_nk(lambda_list).imag)
-plt.plot(nk_data.loc[nk_data['material']=='ITO', ['wl']], 
-         nk_data.loc[nk_data['material']=='ITO', ['n']],'bo')
-plt.plot(nk_data.loc[nk_data['material']=='ITO', ['wl.1']], 
-         nk_data.loc[nk_data['material']=='ITO', ['k']],'ro')
-plt.xlabel('Wavelength ($\mu$m)')
-plt.xlim([0.3, 15])
-plt.xscale('symlog', linthresh=1)
-plt.xticks(tick_list, tick_labels)
-plt.ylabel('Material n,k')
-plt.ylim([0, 5])
-plt.title('ITO')
-plt.show()
